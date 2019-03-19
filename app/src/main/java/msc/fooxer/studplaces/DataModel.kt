fe66eb3b5b6a7f888d1fefe39dfb1c3c2b4dc7cf
Yolley77 <Ruslan5-5@ya.ru>
2019-03-19 18:43:23 +0300

    Prototypes of list elements works correctly

--- app/src/main/java/msc/fooxer/studplaces/DataModel.kt
+++ app/src/main/java/msc/fooxer/studplaces/DataModel.kt
@@ -1,6 +1,5 @@
 package msc.fooxer.studplaces
 
-class DataModel (image: Int, text: String) {
-    var image : Int = R.drawable.samurai
-    var text : String = "Qwertyuiop[]"
+class DataModel (var image: Int = R.drawable.samurai, var text: String = "HI CAT") {
+
 }
