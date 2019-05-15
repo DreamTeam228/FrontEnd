2cf9bed9b1a4591b6871a068bac5b5bd6f9573ea
Fooxer <fooxer9@users.noreply.github.com>
2019-05-16 01:35:13 +0300

    Update SplashScreen.kt

--- app/src/main/java/msc/fooxer/studplaces/SplashScreen.kt
+++ app/src/main/java/msc/fooxer/studplaces/SplashScreen.kt
@@ -18,7 +18,7 @@ import java.net.URL
 import java.util.ArrayList
 
 class SplashScreen : AppCompatActivity() {
-    var dp: ArrayList<Place> = ArrayList()
+    var dp : ArrayList<Place> = ArrayList()
 
     //var bp: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,13 +28,13 @@ class SplashScreen : AppCompatActivity() {
     }
 
 
-    inner class AsynkJson(private val context: Context) : AsyncTask<Void, Void, String>() {
+    inner class AsynkJson(private val context: Context): AsyncTask<Void, Void, String>()
+    {
         var json_url: String = ""
         override fun onPreExecute() {
             json_url = "http://trportal.ru/nekit/get_places.php"
             if (!isNetworkAvailable(context)) {
-                Toast.makeText(context, "The connection is lost", Toast.LENGTH_LONG)
-                    .show() //вывод сообщения о соединении с интернетом
+                Toast.makeText(context, "The connection is lost", Toast.LENGTH_LONG).show() //вывод сообщения о соединении с интернетом
             } else Toast.makeText(context, "The connection is ok", Toast.LENGTH_LONG).show()
         }
 
@@ -77,11 +77,26 @@ class SplashScreen : AppCompatActivity() {
             }
         }
 
+        override fun onPostExecute(result: String) {
+            super.onPostExecute(result)
+            try {
+                dp = MainActivity.pla.getInfo(result)
+            } catch (e: Exception) {
+                Toast.makeText(context, print(e.message).toString(), Toast.LENGTH_LONG).show()
+            }
+            if(dp.isNotEmpty())
+                Toast.makeText(context, "Data is downloaded", Toast.LENGTH_LONG).show() //ok
 
-        private fun isNetworkAvailable(context: Context): Boolean {
-            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            val networkInfo = connectivityManager.activeNetworkInfo
-            return networkInfo != null && networkInfo.isConnectedOrConnecting
+            val i = Intent(baseContext, MainActivity::class.java)
+            i.putParcelableArrayListExtra("dp_ELEMENTS", dp)
+            startActivity(i)
+            finish()
         }
     }
+}
+
+private fun isNetworkAvailable(context: Context): Boolean {
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val networkInfo = connectivityManager.activeNetworkInfo
+    return networkInfo != null && networkInfo.isConnectedOrConnecting
 }
